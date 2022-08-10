1c7141579b6bc707fd6bf9f72b893891d296dac2
3-201412419 <dok2andtablo@naver.com>
2022-08-10 23:44:23 +0900

    내용

---
+++ pages/posts/show_img.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Profile  = () => (
+    <Image
+    src = "/image/문제이스.png"
+    height =  {200}
+    width = {300}
+    alt = "ginger"
+    
+    ></Image>
+);
+export default Profile;
